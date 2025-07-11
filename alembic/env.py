cdd086098092a28d8ceea7375816fbb55fbddc61
alexvodyanitsky <alexvod550@yandex.ru>
2025-07-11 05:27:48 +0300

    migrate to poetry, add base enrichers, integrate alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,10 @@
+from dotenv import load_dotenv
 from logging.config import fileConfig
-
-from sqlalchemy import pool
+import os
 
 from alembic import context
-from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 
-
-import os
-from sqlalchemy import engine_from_config
 from ..src.models.models import Base
 
 
